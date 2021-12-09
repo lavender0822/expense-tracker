6467b17bb4eca6b216943b797de29bb987c9736a
lavender <id0421942@gmail.com>
2021-12-09 17:13:57 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 
+require('./config/mongoose')
+
 const app = express();
 
 const PORT = 3000 
 
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs');
+
+
 
 app.get('/', (req, res) => {
     res.render('index')

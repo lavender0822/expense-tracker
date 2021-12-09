7361c338caf1ae46cebebe3724aaba2766a90b5d
lavender <id0421942@gmail.com>
2021-12-09 19:54:53 +0800

    feat: add method-override and edit page and delete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 
 const router = require('./routes')
 
@@ -13,6 +14,7 @@ app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs');
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(router)
 
--- routes/modules/records.js
+++ routes/modules/records.js
@@ -12,11 +12,43 @@ router.post('/', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+
 router.get('/:id', (req, res) => {
     const id = req.params.id
     Record.findById(id)
     .lean()
     .then(record => res.render('detail', { record }))
+    .catch(error => console.log(error))
+})
+
+router.get('/:id/edit', (req, res) => {
+    const id = req.params.id
+    Record.findById(id)
+    .lean()
+    .then(record => res.render('edit', { record }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+    const id = req.params.id
+    const { name, date, category, amount } = req.body
+    return Record.findById(id)
+    .then(record => {
+        record.name  = name
+        record.date = date
+        record.category = category
+        record.amount = amount
+        return record.save()
+    } )
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id
+    Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 

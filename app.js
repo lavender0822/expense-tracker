85ac6773a43741f3ff08c9f8ad757d4de6c1b28a
lavender <id0421942@gmail.com>
2021-12-09 17:03:12 +0800

    feat: add exphbs

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const exphbs = require('express-handlebars')
 
 const app = express();
 
 const PORT = 3000 
 
+app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', 'hbs');
+
 app.get('/', (req, res) => {
-    res.send('GOOD')
+    res.render('index')
 })
 
 app.listen(PORT, () => {

3a4dbeb477d67f8b95fa00d3d86f569bf13187da
lavender <id0421942@gmail.com>
2021-12-09 17:42:29 +0800

    feat: add routes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const exphbs = require('express-handlebars')
 
+const router = require('./routes')
+
 require('./config/mongoose')
 
 const app = express();
@@ -10,11 +12,7 @@ const PORT = 3000
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs');
 
-
-
-app.get('/', (req, res) => {
-    res.render('index')
-})
+app.use(router)
 
 app.listen(PORT, () => {
     console.log('listening on port')
---
+++ routes/modules/home.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const Record = require('../../models/record')
+
+router.get('/', (req, res) => {
+    Record.find()
+    .lean()
+    .then(records => res.render('index', { records }))
+    .catch(err => console.log(error))
+})
+
+
+
+
+
+module.exports = router

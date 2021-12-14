14bd7caeb790c92be96b0ed7cbf5702b12bc6515
lavender <id0421942@gmail.com>
2021-12-14 17:10:07 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
-const express = require('express');
+const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
-const session = require('express-session')
+
 
 const router = require('./routes')
 
+const userpPassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express();
 
-const PORT = process.env.PORT || 3000 
+const PORT = process.env.PORT || 3000
 
-app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs');
 
 app.use(session({
     secret: 'ThisIsMySecret',
     resave: false,
     saveUninitialized: true
-  }))
-  
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+userpPassport(app)
 
 app.use(router)
 

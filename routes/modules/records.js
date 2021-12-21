fb6e04d787ba64ba20a9640d80a56b2ed86b9b18
lavender <id0421942@gmail.com>
2021-12-21 20:25:35 +0800

    調整

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -1,48 +1,70 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/new',  (req, res) => {
-    res.render('new')
+
+//  新增
+router.get('/new', (req, res) => {
+  Category.find()
+    .lean()
+    .then((categories) => res.render('new', { categories }))
+    .catch((error) => console.error(error))
 })
 
-router.post('/', (req, res) => {
-    const userId = req.user._id
-    const {  name, date, category, amount } = req.body
-    return Record.create({ name, date, category, amount, userId })
+router.post('/new', (req, res) => {
+  const { name, date, category, amount } = req.body
+  const userId = req.user._id
+  return Category.findOne({ name_cn: category })
+    .then(category => {
+      Record.create({
+        name,
+        date,
+        category,
+        amount,
+        userId,
+      })
+    })
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
-
+//  修改
 router.get('/:id/edit', (req, res) => {
-    const userId = req.user._id
-    const _id = req.params.id
-    return  Record.findOne({ _id, userId })
+  const userId = req.user._id
+  const _id = req.params.id
+  const categoryList = []
+  Category.find()
     .lean()
-    .then(record => res.render('edit', { record }))
-    .catch(error => console.log(error))
+    .then((items) => {
+      items.forEach((item) => categoryList.push(item.name_cn))
+    })
+  return Record.findOne({ _id, userId })
+    .lean()
+    .then((record) => res.render('edit', { record, categoryList }))
 })
-
-router.put('/:id', (req, res) => {
-    const userId = req.user._id
-    const _id = req.params.id
-    const { name, date, category, amount } = req.body
-    return Record.findOne({ _id, userId })
-    .then(record => {
-        record.name  = name
-        record.date = date
-        record.category = category
-        record.amount = amount
-        return record.save()
-    } )
-    .then(() => res.redirect(`/records/${_id}`))
-    .catch(error => console.log(error))
+router.put('/:id/', (req, res) => {
+  const userId = req.user._id
+  const _id = req.params.id
+  const { name, date, category, amount } = req.body
+  return Category.findOne({ name_cn: category })
+    .then(category => {
+      Record.findOne({ _id, userId })
+        .then(record => {
+          record.name = name
+          record.date = date
+          record.amount = amount
+          record.category = category
+          return record.save()
+        })
+        .then(() => res.redirect('/'))
+        .catch(error => console.log(error))
+    })
 })
-
+//  刪除
 router.delete('/:id', (req, res) => {
-    const userId = req.user._id
-    const _id = req.params.id
-    return Record.findOne({ _id, userId })
+  const userId = req.user._id
+  const _id = req.params.id
+  return Record.findOne({ _id, userId })
     .then(record => record.remove())
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))

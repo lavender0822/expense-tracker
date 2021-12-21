aa8688a3b799816040c1001384887dea0d60428d
lavender <id0421942@gmail.com>
2021-12-21 20:05:28 +0800

    feat: add icon

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-  }
+}
 
 const router = require('./routes')
 
@@ -17,7 +17,12 @@ const app = express();
 
 const PORT = process.env.PORT
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: require('./config/helpers')
+}))
+
 app.set('view engine', 'hbs');
 
 app.use(session({
@@ -35,7 +40,7 @@ app.use(flash())
 app.use((req, res, next) => {
     res.locals.isAuthenticated = req.isAuthenticated()
     res.locals.user = req.user
-    res.locals.success_msg = req.flash('success_msg')  
+    res.locals.success_msg = req.flash('success_msg')
     res.locals.warning_msg = req.flash('warning_msg')
     next()
 })
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,14 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
+const Category = require('../../models/category')
 
-router.get('/', (req, res) => {
+router.get('/filter', async (req, res) => {
+    const userId = req.user._id
+    const category = req.query.category
+    const filteredCategory = (req.query.category === 'all') ? '' : await Category.findOne({ name: req.query.category }).lean()
+    const filter = (filteredCategory === '') ? { userId } : { userId, category: filteredCategory._id }
+    Record.find(filter)
+      .populate('category')
+      .lean()
+      .then(records => res.render('index', { records, category }))
+      .catch(error => console.error(error))
+  })
+  
+  
+  
+  router.get('/', (req, res) => {
     const userId = req.user._id
     Record.find({ userId })
-    .lean()
-    .sort({ date: 'asc'})
-    .then(records => res.render('index', { records }))
-    .catch(err => console.log(error))
-})
-
-module.exports = router
+      .lean()
+      .populate('category')
+      .sort({ _id: 'desc' })
+      .then(records => { res.render('index', { records }) })
+      .catch(error => console.log(error))
+  })
+  module.exports = router
